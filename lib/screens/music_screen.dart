cd8678bdcd9ecf61c77353c7d92f4fb90981a48d
Sajjad Leith <sajjadleith@Sajjads-MacBook-Pro.local>
2025-09-21 11:00:17 +0300

    making the slider and make it play and replay

--- lib/screens/music_screen.dart
+++ lib/screens/music_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import '../core/app-constain.dart';
 import '../core/widgets/custom-card.dart';
@@ -11,6 +13,60 @@ class MusicScreen extends StatefulWidget {
 }
 
 class _MusicScreenState extends State<MusicScreen> {
+  double currentValue = 0.0;
+  final double maxValue = 300;
+
+  String formatTime(int second) {
+    final minutes = (second ~/ 60).toString();
+    final secs = (second % 60).toString().padLeft(2, '0');
+    return "$minutes:$secs";
+  }
+  Timer? _timer;
+
+  @override
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (currentValue < maxValue) {
+        setState(() {
+          currentValue += 1;
+        });
+      } else {
+        timer.cancel();
+      }
+    });
+  }
+
+  void startTimer() {
+    _timer?.cancel(); // نوقف أي تايمر موجود
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (currentValue < maxValue) {
+        setState(() {
+          currentValue += 1;
+        });
+      } else {
+        timer.cancel();
+      }
+    });
+  }
+
+  void resetTimer() {
+    _timer?.cancel();
+    setState(() {
+      currentValue = 0;
+    });
+  }
+
+
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,176 +107,209 @@ class _MusicScreenState extends State<MusicScreen> {
                 width: MediaQuery.of(context).size.width,
                 child: Container(
                   width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.sizeOf(context).height,
                   decoration: BoxDecoration(
                     color: Colors.grey[300],
                     borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(30),
                     ),
                   ),
-                  child: Column(
-                    children: [
-                      Container(
-                        padding: EdgeInsets.only(left: 20, top: 30, bottom: 5),
-                        child: Text(
-                          "Daily Meditation",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25,
-                          ),
-                        ),
-                      ),
-                      SizedBox(height: 30),
-
-                      Container(
-                        width: 200,
-                        height: 200,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                            image: AssetImage(AppConstain.env4),
-                            fit: BoxFit.fill,
-                          ),
-                        ),
-                      ),
-
-                      Container(
-                        padding: EdgeInsets.only(left: 10, top: 20, bottom: 5),
-                        child: Text(
-                          "River Sound",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25,
-                          ),
-                        ),
-                      ),
-
-                      SizedBox(height: 30),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Container(
-                            padding: EdgeInsets.only(left: 50),
-                            child: Material(
-                              shape: CircleBorder(
-                                side: BorderSide(color: Colors.black),
-                              ),
-                              color: Colors.white,
-                              child: InkWell(
-                                onTap: () {},
-                                customBorder: CircleBorder(),
-                                child: Padding(
-                                  padding: EdgeInsets.all(17),
-                                  child: SvgPicture.asset(
-                                    AppConstain.replay,
-                                    height: 18,
-                                    colorFilter: const ColorFilter.mode(
-                                      Colors.black,
-                                      BlendMode.srcIn,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Material(
-                            shape: CircleBorder(),
-                            color: Colors.purple[900],
-                            child: InkWell(
-                              onTap: () {},
-                              customBorder: CircleBorder(),
-                              child: Padding(
-                                padding: EdgeInsets.all(17),
-                                child: SvgPicture.asset(
-                                  AppConstain.play,
-                                  height: 15,
-                                  colorFilter: const ColorFilter.mode(
-                                    Colors.white,
-                                    BlendMode.srcIn,
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(right: 50),
-                            child: Material(
-                              shape: CircleBorder(
-                                side: BorderSide(color: Colors.black),
-                              ),
-                              color: Colors.white,
-                              child: InkWell(
-                                onTap: () {},
-                                customBorder: CircleBorder(),
-                                child: Padding(
-                                  padding: EdgeInsets.all(17),
-                                  child: SvgPicture.asset(
-                                    AppConstain.love,
-                                    height: 18,
-                                    colorFilter: const ColorFilter.mode(
-                                      Colors.black,
-                                      BlendMode.srcIn,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 30),
-                      Column(
-                        children: [
-                          Slider(
-                            value: 2.24,
-                            min: 0,
-                            max: 3.00,
-                            activeColor: Colors.purple[900],
-                            inactiveColor: Colors.grey,
-                            onChanged: (value) {},
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: const [
-                                Text("2:24"),
-                                Text("3:00"),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-
-                      Padding(
-                        padding: const EdgeInsets.only(
-                          left: 30,
-                          right: 30,
-                          top: 30,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Rekomendasi",
+                  child: CustomScrollView(
+                    slivers: [
+                      SliverToBoxAdapter(
+                        child: Center(
+                          child: Container(
+                            padding: EdgeInsets.only(left: 20, top: 30, bottom: 5),
+                            child: Text(
+                              "Daily Meditation",
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
-                                fontSize: 20,
+                                fontSize: 25,
                               ),
                             ),
-                            Text(
-                              "Lihat Lebih",
-                              style: TextStyle(color: Colors.blueAccent),
+                          ),
+                        ),
+                      ),
+                      SliverToBoxAdapter(child: SizedBox(height: 30)),
+
+                      SliverToBoxAdapter(
+                        child: Container(
+                          width: 200,
+                          height: 200,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                              image: AssetImage(AppConstain.env4),
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      SliverToBoxAdapter(
+                        child: Center(
+                          child: Container(
+                            padding: EdgeInsets.only(left: 10, top: 20, bottom: 5),
+                            child: Text(
+                              "River Sound",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      SliverToBoxAdapter(child: SizedBox(height: 30)),
+                      SliverToBoxAdapter(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.only(left: 50),
+                              child: Material(
+                                shape: CircleBorder(
+                                  side: BorderSide(color: Colors.black),
+                                ),
+                                color: Colors.white,
+                                child: InkWell(
+                                  onTap: () {
+                                    resetTimer();
+                                  },
+                                  customBorder: CircleBorder(),
+                                  child: Padding(
+                                    padding: EdgeInsets.all(17),
+                                    child: SvgPicture.asset(
+                                      AppConstain.replay,
+                                      height: 18,
+                                      colorFilter: const ColorFilter.mode(
+                                        Colors.black,
+                                        BlendMode.srcIn,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Material(
+                              shape: CircleBorder(),
+                              color: Colors.purple[900],
+                              child: InkWell(
+                                onTap: () {
+                                  startTimer();
+                                },
+                                customBorder: CircleBorder(),
+                                child: Padding(
+                                  padding: EdgeInsets.all(17),
+                                  child: SvgPicture.asset(
+                                    AppConstain.play,
+                                    height: 40,
+                                    colorFilter: const ColorFilter.mode(
+                                      Colors.white,
+                                      BlendMode.srcIn,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: EdgeInsets.only(right: 50),
+                              child: Material(
+                                shape: CircleBorder(
+                                  side: BorderSide(color: Colors.black),
+                                ),
+                                color: Colors.white,
+                                child: InkWell(
+                                  onTap: () {},
+                                  customBorder: CircleBorder(),
+                                  child: Padding(
+                                    padding: EdgeInsets.all(17),
+                                    child: SvgPicture.asset(
+                                      AppConstain.love,
+                                      height: 18,
+                                      colorFilter: const ColorFilter.mode(
+                                        Colors.black,
+                                        BlendMode.srcIn,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SliverToBoxAdapter(child: SizedBox(height: 30)),
+                      SliverToBoxAdapter(
+                        child: Column(
+                          children: [
+                            Slider(
+                              value: currentValue,
+                              min: 0,
+                              max: maxValue,
+                              activeColor: Colors.purple[900],
+                              inactiveColor: Colors.grey,
+                              onChanged: (value) {
+                                currentValue = value;
+                              },
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(formatTime(currentValue.toInt())),
+                                  Text(formatTime(300)),
+                                ],
+                              ),
                             ),
                           ],
                         ),
                       ),
 
-                      CustomCart(
-                        title: 'Meditasi Dasar 2',
-                        desc: '2 Menit . Singkart',
-                        img: '${AppConstain.imgs[0]}',
-                        onTap: () {},
+                      SliverToBoxAdapter(
+                        child: Padding(
+                          padding: const EdgeInsets.only(
+                            left: 30,
+                            right: 30,
+                            top: 30,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Rekomendasi",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              Text(
+                                "Lihat Lebih",
+                                style: TextStyle(color: Colors.blueAccent),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+
+                      SliverToBoxAdapter(
+                        child: CustomCart(
+                          title: 'Meditasi Dasar 2',
+                          desc: '2 Menit . Singkart',
+                          img: '${AppConstain.imgs[0]}',
+                          onTap: () {},
+                        ),
+                      ),
+                      SliverToBoxAdapter(
+                        child: CustomCart(
+                          title: 'Meditasi Dasar 2',
+                          desc: '2 Menit . Singkart',
+                          img: '${AppConstain.imgs[0]}',
+                          onTap: () {},
+                        ),
                       ),
                     ],
                   ),
