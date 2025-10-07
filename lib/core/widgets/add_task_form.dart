b1529733f1dc3dbee01b49e5c295a530e210be85
Sajjad Leith <sajjadleith@Sajjads-MacBook-Pro.local>
2025-10-07 05:21:53 +0300

    making edit to the todo task and showing the time and date in the card

--- lib/core/widgets/add_task_form.dart
+++ lib/core/widgets/add_task_form.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:music_app/models/task_model.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddTaskForm extends StatefulWidget {
   final Function(TaskModel) onAdd;
-  AddTaskForm({super.key, required this.onAdd});
+  final TaskModel? existingTask;
+  AddTaskForm({super.key, required this.onAdd, this.existingTask});
 
   @override
   _AddTaskFormState createState() => _AddTaskFormState();
@@ -16,6 +20,21 @@ class _AddTaskFormState extends State<AddTaskForm> {
   final TextEditingController _timeController = TextEditingController();
   final TextEditingController _dateController = TextEditingController();
 
+  File? _imageFile;
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _pickeImage() async {
+    final pickFile = await _picker.pickImage(
+      source: ImageSource.gallery,
+      imageQuality: 70,
+    );
+    if (pickFile != null) {
+      setState(() {
+        _imageFile = File(pickFile.path);
+      });
+    }
+  }
+
   void _submit() {
     if (_formKey.currentState!.validate()) {
       final newTask = TaskModel(
@@ -23,9 +42,28 @@ class _AddTaskFormState extends State<AddTaskForm> {
         desc: _descController.text,
         time: _timeController.text,
         date: _dateController.text,
+        img: _imageFile?.path,
       );
-      widget.onAdd(newTask);
+      if (widget.existingTask != null) {
+        widget.onAdd(newTask);
+      } else {
+        widget.onAdd(newTask);
+      }
       Navigator.pop(context);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.existingTask != null) {
+      _titleController.text = widget.existingTask!.title;
+      _descController.text = widget.existingTask!.desc ?? "";
+      _dateController.text = widget.existingTask!.date ?? "";
+      _timeController.text = widget.existingTask!.time ?? "";
+      if (widget.existingTask!.img != null) {
+        _imageFile = File(widget.existingTask!.img!);
+      }
     }
   }
 
@@ -33,30 +71,51 @@ class _AddTaskFormState extends State<AddTaskForm> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextFormField(
-            controller: _titleController,
-            decoration: InputDecoration(labelText: "Title"),
-            validator: (value) =>
-                value!.isEmpty ? "Please Enter the Title" : null,
-          ),
-          TextFormField(
-            controller: _descController,
-            decoration: InputDecoration(labelText: "Description"),
-          ),
-          TextFormField(
-            controller: _timeController,
-            decoration: InputDecoration(labelText: "Time"),
-          ),
-          TextFormField(
-            controller: _dateController,
-            decoration: InputDecoration(labelText: "Date"),
-          ),
-          SizedBox(height: 20),
-          ElevatedButton(onPressed: _submit, child: Text("Add Task")),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              controller: _titleController,
+              decoration: InputDecoration(labelText: "Title"),
+              validator: (value) =>
+                  value!.isEmpty ? "Please Enter the Title" : null,
+            ),
+            TextFormField(
+              controller: _descController,
+              decoration: InputDecoration(labelText: "Description"),
+            ),
+            TextFormField(
+              controller: _timeController,
+              decoration: InputDecoration(labelText: "Time"),
+            ),
+            TextFormField(
+              controller: _dateController,
+              decoration: InputDecoration(labelText: "Date"),
+            ),
+            SizedBox(height: 20),
+            if (_imageFile != null)
+              Image.file(
+                _imageFile!,
+                width: 100,
+                height: 100,
+                fit: BoxFit.cover,
+              ),
+            const SizedBox(height: 10),
+            ElevatedButton.icon(
+              onPressed: _pickeImage,
+              icon: Icon(Icons.photo),
+              label: Text("Pick Image"),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _submit,
+              child: Text(
+                widget.existingTask != null ? "Edit Task" : "Add Task",
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/active_tasks_screen.dart
+++ lib/screens/active_tasks_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:music_app/core/widgets/add_task_form.dart';
 import 'package:music_app/models/task_model.dart';
@@ -25,11 +27,32 @@ class _ActiveTasksScreenState extends State<ActiveTasksScreen> {
 
   // Update Task
 
-  void _updateTask(TaskModel task) {
-    task.status = task.status == TaskStatus.pending
-        ? TaskStatus.done
-        : TaskStatus.pending;
-    setState(() {});
+  void _editTask(TaskModel task, int index) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true,
+      builder: (context) {
+        return Container(
+          padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          width: MediaQuery.sizeOf(context).width,
+          height: 400,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(20),
+              topRight: Radius.circular(20),
+            ),
+          ),
+          child: AddTaskForm(
+            existingTask: task,
+            onAdd: (updatedTask) {
+              setState(() {
+                tasks[index] = updatedTask;
+              });
+            },
+          ),
+        );
+      },
+    );
   }
 
   void _openAddTaskModel() {
@@ -68,24 +91,37 @@ class _ActiveTasksScreenState extends State<ActiveTasksScreen> {
                 return Card(
                   child: ListTile(
                     leading: activeTasks[index].img != null
-                        ? Image.network(
-                            activeTasks[index].img!,
+                        ? Image.file(
+                            File(activeTasks[index].img!),
                             width: 50,
                             fit: BoxFit.cover,
                           )
                         : Icon(Icons.task),
-                    title: Text(activeTasks[index].title),
-                    subtitle: Text(activeTasks[index].desc ?? ""),
+                    title: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(activeTasks[index].title),
+                        Text(
+                          activeTasks[index].time ?? "0:0",
+                          style: TextStyle(fontSize: 12),
+                        ),
+                      ],
+                    ),
+                    subtitle: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(activeTasks[index].desc ?? ""),
+                        Text(activeTasks[index].date ?? ""),
+                      ],
+                    ),
                     trailing: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: [
                         IconButton(
-                          icon: Icon(
-                            activeTasks[index].status == TaskStatus.done
-                                ? Icons.check_circle
-                                : Icons.circle_outlined,
-                          ),
-                          onPressed: () => _updateTask(activeTasks[index]),
+                          icon: Icon(Icons.edit),
+                          onPressed: () => _editTask(activeTasks[index], index),
                         ),
                         IconButton(
                           icon: const Icon(Icons.delete),
